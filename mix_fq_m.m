1d416edce44ddcc6e1fd1c15f3891b0b95f481fe
my88899 <haojiutao@gmail.com>
2016-08-02 22:14:39 +0800

    3D-dram | figure out the point

--- mix_fq_m.m
+++ mix_fq_m.m
@@ -3,7 +3,7 @@ ts=0.000001;    %²ÉÑù¼ä¸ô
 sweep=10000/0.1;
 c=3e1;
 
-delay_1=0.012;
+delay_1=0.042;
 delay_2=0.037;
 delay_3=0.057;
 
@@ -19,14 +19,22 @@ for i=2:4
 end
 r_s=(ff/sweep)*c;
 
-p={[0 -1 0],[0 1 0],[0 0 1]};
+p={[0 -0.7 0],[0 0.7 0],[0 0 -0.7]};
+color={'r','b','y'};
+
 figure
 hold on
+grid on
+xlabel('x');ylabel('y');zlabel('z');
 for i=1:3
     [x,y,z]=sphere(30);
     X=x*r_s(i)+p{i}(1);
     Y=y*r_s(i)+p{i}(2);
     Z=z*r_s(i)+p{i}(3);
-    mesh(X,Y,Z)
+    w=find(X<0);
+    Z(w)=0;
+    Y(w)=0;
+    X(w)=0;
+    mesh(X,Y,Z,'FaceColor',color{i})
 end
 hold off
